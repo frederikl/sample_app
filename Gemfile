7cd60673ad1dcf8a9235a5f6e58481df0c3001e0
Frederik Laulund <frederik@intraz.com>
2014-06-11 10:55:02 +0000

    Finished user edit, update, index and destroy actions (ch. 9)

--- Gemfile
+++ Gemfile
@@ -7,17 +7,20 @@ gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
 # gem 'pg', '0.15.1'
+gem 'minitest'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', '~> 2.14.0.rc1' #'2.13.1'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
-  gem 'minitest'
   gem 'factory_girl_rails', '4.2.1'
 end
 
@@ -43,7 +46,7 @@ group :doc do
 end
 
 
-  
+
 group :production do
 	gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-
-  	if user && user.authenticate(params[:session][:password])
-  		# sign in the user and redirect to user profile (show form).
-  		sign_in user
-  		redirect_to user
-  	else
-  		# Create error message and rerender signin form.
-  		flash.now[:error] = 'Invalid email/password combination'
-  		render 'new'
-  	end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
     sign_out
     redirect_to root_url
   end
-  
+
 end
